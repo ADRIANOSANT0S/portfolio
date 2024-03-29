c2b36072d90bed56d8921fa992caca3ed534e605
Adriano <adrianoopereiira@hotmail.com>
2024-03-29 20:02:16 -0300

    fix: resolvendo a exibição do botão e as imagem do site que não estava mostrando

--- src/views/Collections/SkillsData/Skills.js
+++ src/views/Collections/SkillsData/Skills.js
@@ -1,82 +1,82 @@
 export default [
   {
-    image: '/src/assets/images/skills/html.png',
+    image: './src/assets/images/skills/html.png',
     title: 'HTML'
   },
   {
-    image: '/src/assets/images/skills/git.png',
+    image: './src/assets/images/skills/git.png',
     title: 'Git'
   },
   {
-    image: '/src/assets/images/skills/github.png',
+    image: './src/assets/images/skills/github.png',
     title: 'Github'
   },
   {
-    image: '/src/assets/images/skills/javascript.png',
+    image: './src/assets/images/skills/javascript.png',
     title: 'Javascript'
   },
   {
-    image: '/src/assets/images/skills/typescript.webp',
+    image: './src/assets/images/skills/typescript.webp',
     title: 'Typescript'
   },
   {
-    image: '/src/assets/images/skills/Python.png',
+    image: './src/assets/images/skills/Python.png',
     title: 'Python'
   },
   {
-    image: '/src/assets/images/skills/css.png',
+    image: './src/assets/images/skills/css.png',
     title: 'CSS'
   },
   {
-    image: '/src/assets/images/skills/sass.png',
+    image: './src/assets/images/skills/sass.png',
     title: 'SASS'
   },
   {
-    image: '/src/assets/images/skills/less.webp',
+    image: './src/assets/images/skills/less.webp',
     title: 'LESS'
   },
   {
-    image: '/src/assets/images/skills/bootstrap.png',
+    image: './src/assets/images/skills/bootstrap.png',
     title: 'Bootstrap'
   },
   {
-    image: '/src/assets/images/skills/gulp.png',
+    image: './src/assets/images/skills/gulp.png',
     title: 'Gulp'
   },
   {
-    image: '/src/assets/images/skills/grunt.png',
+    image: './src/assets/images/skills/grunt.png',
     title: 'Grunt'
   },
   {
-    image: '/src/assets/images/skills/parcel.png',
+    image: './src/assets/images/skills/parcel.png',
     title: 'Parcel'
   },
   {
-    image: '/src/assets/images/skills/jquery.png',
+    image: './src/assets/images/skills/jquery.png',
     title: 'jQuery'
   },
   {
-    image: '/src/assets/images/skills/vue.png',
+    image: './src/assets/images/skills/vue.png',
     title: 'Vue'
   },
   {
-    image: '/src/assets/images/skills/react.svg',
+    image: './src/assets/images/skills/react.svg',
     title: 'React'
   },
   {
-    image: '/src/assets/images/skills/redux.svg',
+    image: './src/assets/images/skills/redux.svg',
     title: 'Redux'
   },
   {
-    image: '/src/assets/images/skills/stylecomponent.jpg',
+    image: './src/assets/images/skills/stylecomponent.jpg',
     title: 'Style Component'
   },
   {
-    image: '/src/assets/images/skills/cypress.png',
+    image: './src/assets/images/skills/cypress.png',
     title: 'Cypress'
   },
   {
-    image: '/src/assets/images/skills/ftp.jpg',
+    image: './src/assets/images/skills/ftp.jpg',
     title: 'FTP'
   }
 ]
